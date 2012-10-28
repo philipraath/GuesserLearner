8c4f503f2d6527eaff988e7659b2fca76abd2c07
Philip Raath <philraath@gmail.com>
2012-10-28 09:18:01 -0700

    implemented GuesserLearnerTUI and GuesserLearnerTUITest

---
+++ src/GuesserLearnerTUI.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author philraath
+ *
+ */
+public class GuesserLearnerTUI extends AbstractGuesserLearnerTUI {
+
+	static Scanner inputReader = new Scanner(System.in);
+	/* 
+	 * @see UIInterface#displayWelcome()
+	 */
+	@Override
+	public void displayWelcome() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see UIInterface#displayGoodbye()
+	 */
+	@Override
+	public void displayGoodbye() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see UIInterface#displayQuestion()
+	 */
+	@Override
+	public String askQuestion(String question) {
+		return null;
+		// TODO Auto-generated method stub
+	}
+
+	/* 
+	 * @see UIInterface#displayStatment()
+	 */
+	@Override
+	public void displayStatment(String statement) {
+		// TODO Auto-generated method stub
+
+	}
+}
